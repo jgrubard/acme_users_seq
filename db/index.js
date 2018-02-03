038d91952cabd01ecf7f9f25269a1d8d4f6b6d4e
Jeremy Grubard <jgrubard@gmail.com>
2018-02-03 15:53:43 -0500

    isolate sequelize to db/index.js

---
+++ db/index.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const _conn = new Sequelize(process.env.DATABASE_URL);
+
+const User = _conn.define('user', { // create table guidelines
+  name: Sequelize.STRING
+});
+
+const sync = () => {
+  return _conn.sync({ force: true })
+};
+
+const seed = () => {
+  return Promise.all([
+    User.create({ name: 'Cersei' }),
+    User.create({ name: 'Jaime' }),
+    User.create({ name: 'Tyrion' })
+  ]);
+};
+
+module.exports = {
+  sync,
+  seed
+};
--- server.js
+++ server.js
@@ -1,29 +1,14 @@
 const express = require('express');
 const app = express();
+const db = require('./db');
 
 const port = process.env.Port || 3000;
-
-const Sequelize = require('sequelize');
-const _conn = new Sequelize(process.env.DATABASE_URL);
-
-const User = _conn.define('user', { // create table guidelines
-  name: Sequelize.STRING
-});
-
-_conn.sync({ force: true })
-  .then( () => {
-    return Promise.all([
-      User.create({ name: 'Cersei' }),
-      User.create({ name: 'Jaime' }),
-      User.create({ name: 'Tyrion' })
-    ]);
-  }).then( (users) => {
-    return User.findAll({ where: { name: 'Jaime' }})
-  }).then( (users) => {
-    console.log(users[0].name);
-  });
 
 app.listen(port, () => {
   console.log(`** Listening on Port ${port} **`);
 });
 
+db.sync()
+  .then( () => {
+    db.seed();
+  });
